19512d1c2ccdf943eeadd80e0fcfd7d7bdccd078
Divyansh Gupta <divyansh@rysyl.com>
2022-07-21 21:39:13 +0530

    :art: Structured Directories
    created a src folder and added support for relative paths

--- babel.config.js
+++ babel.config.js
@@ -2,5 +2,21 @@ module.exports = function(api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
+    plugins: [
+      [
+        'module-resolver',
+        {
+          "root": ["./src"],
+          alias: {
+            assets: './assets',
+            components: './src/components',
+            modules: './src/modules',
+            lib: './src/lib',
+            types: './src/types',
+            constants: './src/constants',
+          },
+        },
+      ],
+    ],
   };
 };
--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-
+import tw from 'twrnc';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>🌟 Open up App.js to start working on your app!</Text>
+      <Text style={tw`font-bold text-lg`}>🌟 This is a short video consuming app 🦑</Text>
       <StatusBar style="auto" />
     </View>
   );
