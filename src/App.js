44cae957917576d059c34007aba2e0ca07519292
Divyansh Gupta <divyansh@rysyl.com>
2022-07-21 23:45:45 +0530

    :children_crossing: Hello World Button
    added hello world button in App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
 import tw from 'twrnc';
+
 export default function App() {
   return (
     <View style={styles.container}>
       <Text style={tw`font-bold text-lg`}>🌟 This is a short video consuming app 🦑</Text>
+      <TouchableOpacity style={tw`border-blue-300`}>
+        <Text>Hello World 🙌</Text>
+      </TouchableOpacity>
       <StatusBar style="auto" />
     </View>
   );
