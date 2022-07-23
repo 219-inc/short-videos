cb83fd6b03cbe73ed39d8a33b0b34696dbf7ca1a
Divyansh Gupta <divyansh@rysyl.com>
2022-07-23 13:44:31 +0530

    initial setup

--- App.js
+++ App.js
@@ -1,19 +1,10 @@
-import { Amplify } from 'aws-amplify'
-import { NavigationContainer } from '@react-navigation/native';
 import { View } from 'react-native';
 import tw from 'twrnc'
-import awsconfig from './src/aws-exports'
-
-import Root from 'stack/Root';
-
-Amplify.configure(awsconfig)
 
 export default function App() {
   return (
     <View style={tw`h-full`}>
-      <NavigationContainer>
-        <Root />
-      </NavigationContainer>
+      <Text>App.js 🌟</Text>
     </View>
   );
 }
