1a699f2155ce8de3975d0853e91ef8cb0fd3bc04
Divyansh Gupta <divyansh@rysyl.com>
2022-07-23 17:13:57 +0530

    :triangular_flag_on_post: Amplify setup
    Amplify Cognito auth setup

--- src/screens/Login.js
+++ src/screens/Login.js
@@ -1,16 +1,34 @@
-import { View, Text, SafeAreaView } from 'react-native'
+import { View, Text, SafeAreaView, TextInput, TouchableOpacity } from 'react-native'
 import tw from 'twrnc'
 
-import { withAuthenticator } from 'aws-amplify-react-native';
-
-const Login = ({ signOut, user }) => {
+const Login = () => {
   return (
-    <SafeAreaView style={tw`bg-red-300 h-full pt-12 px-4`}>
-      <Text>Login</Text>
-      <h1>Hello {user.username}</h1>
-      <button onClick={signOut}>Sign out</button>
+    <SafeAreaView style={tw`h-full pt-12 px-4 bg-white`}>
+      <Text 
+        style={tw`text-center text-black text-2xl font-semibold mb-4`}
+      >
+        SHAWTY
+      </Text>
+      <View style={tw`my-auto flex flex-col`}>
+        <TextInput 
+          style={tw`bg-gray-100 py-3 px-4 rounded-lg border border-gray-200`}
+          placeholder='Username'/>
+        <TextInput 
+          style={tw`bg-gray-100 py-3 px-4 rounded-lg border border-gray-200 mt-2`}
+          placeholder='Password' 
+          secureTextEntry={true} />
+        <TouchableOpacity 
+          style={tw`bg-black py-3 px-4 rounded-lg mt-2 flex justify-center`}
+          activeOpacity={0.7}  
+        >
+          <Text style={tw`text-center text-white font-semibold`}>Log in</Text>
+        </TouchableOpacity>
+        <TouchableOpacity>
+          <Text style={tw`text-green-400`}>Forgot your password?</Text>
+        </TouchableOpacity>
+      </View>
     </SafeAreaView>
   )
 }
 
-export default withAuthenticator(Login)
+export default Login
